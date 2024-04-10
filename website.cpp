98c143de545541410f90127ce98c009d2323b3b4
tashyiell <166552007+tashyiell@users.noreply.github.com>
2024-04-10 18:28:08 +0200

    Create website.cpp

---
+++ website.cpp
@@ -0,0 +1,42 @@
+projest structure
+
+:{'main.cpp'}
+
+
+neccessary libraries 
+
+:{#include <iostream>
+:{#include <boost/asio.hpp>
+:{#include <boost/algorithm/string.hpp>
+
+
+define request handler 
+
+{std::string handleRequest(const std::string& request) {
+    // Process the request and generate the response
+    std::string response = "HTTP/1.1 200 OK\r\n\r\nHello, World!";
+    return response;
+}}
+
+set up a TCP server 
+
+{void startServer() {
+    boost::asio::io_context ioContext;
+    boost::asio::ip::tcp::acceptor acceptor(ioContext, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 8080));
+while (true) {
+    boost::asio::ip::tcp::socket socket(ioContext);
+    acceptor.accept(socket);
+    std::array<char, 4096> buffer;
+    boost::system::error_code error;
+    size_t bytesRead = socket.read_some(boost::asio::buffer(buffer), error);
+    std::string request(buffer.data(), bytesRead);
+    std::string response = handleRequest(request);
+    boost::asio::write(socket, boost::asio::buffer(response));
+ }
+}
+}
+
+run the sever
+
+int main(){std::cout<< "starting the web service...\n"; 
+     return 0;}

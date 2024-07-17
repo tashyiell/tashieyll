d088e7d3685ffb96c6bf43268e864b4e19e50a01
tashyiell <166552007+tashyiell@users.noreply.github.com>
2024-07-17 18:42:31 +0200

    Update assaingment1.cpp

--- assaingment1.cpp
+++ assaingment1.cpp
@@ -72,50 +72,54 @@ using namespace std;
 
 int main() {
 
-double number1 , number2 , number3 ;
+    double number1, number2, number3 = 0.0; {number3; };
 
-for (int x=0 ; x<1 ; x++ ) {
-    
-   cout<<"ENTER THE FIRST NUMBER :"<<endl<<endl;
-   
-   cin >>number1;
-   
-   if (number1 >= 10 && number1 <= 20) {
-       
-       cout <<":"<<number1 << " IS BETWEEN 10 AND 20 !"<<endl<<endl;
-      
-   } else {cout<<": "<<number1<<" IS NOT EQUAL OR BETWEEN 10 AND 20 !"<<endl<<endl;}
-   
-   for (int y=0 ; y<1 ; y++) {
-        
-        cout<<": ENTER THE SECOND NUMBER :"<<endl<<endl;
-        
-        cin >>number2;
-        
-        if (number2 >= 20 && number2 <= 30) { 
-            
-        double sum = number1 + number2 + number3 ;
-    
-    cout << ": THE SUM = " <<sum<<endl<<endl;
-        
-    } else {cout<<": "<<number2<<" IS NOT EQUAL OR BETWEEN 20 AND 30 !"<<endl<<endl;}
-    
-    for (int z=0 ; z<1 ; z++) {
-        
-        cout <<": ENTER THE THIRD NUMBER"<<endl<<endl;
-        
-        cin>>number3;
-        
-        if (number3 <= 5) {
-            
-            double results =(number1 + number2) * number3;
-            
-            cout<<": THE SUM OF THE FIRST AND THE SECOND NUMBERS MULTIPLIED BY THE THIRD NUMBER IS = "<<results<<endl<<endl;
-        } else { cout<<": "<<number3<<" IS NOT EQUAL OR LESS THAN 5 !"<<endl<<endl;}
+    for (int x = 0; x < 1; x++) {
+
+        cout << "ENTER THE FIRST NUMBER :" << endl << endl;
+
+        cin >> number1;
+
+        if (number1 >= 10 && number1 <= 20) {
+
+            cout << ":" << number1 << " IS BETWEEN 10 AND 20 !" << endl << endl;
+
+        }
+        else { cout << ": " << number1 << " IS NOT EQUAL OR BETWEEN 10 AND 20 !" << endl << endl; }
+
+        for (int y = 0; y < 1; y++) {
+
+            cout << ": ENTER THE SECOND NUMBER :" << endl << endl;
+
+            cin >> number2;
+
+            if (number2 >= 20 && number2 <= 30) {
+
+                double sum = number1 + number2 + number3;
+
+                cout << ": THE SUM = " << sum << endl << endl;
+
+            }
+            else { cout << ": " << number2 << " IS NOT EQUAL OR BETWEEN 20 AND 30 !" << endl << endl; }
+
+            for (int z = 0; z < 1; z++) {
+
+                cout << ": ENTER THE THIRD NUMBER" << endl << endl;
+
+                cin >> number3;
+
+                if (number3 <= 5) {
+
+                    double results = (number1 + number2) * number3;
+
+                    cout << ": THE SUM OF THE FIRST AND THE SECOND NUMBERS MULTIPLIED BY THE THIRD NUMBER IS "<<" = " << results << endl << endl;
+                }
+                else { cout << ": " << number3 << " IS NOT EQUAL OR LESS THAN 5 !" << endl << endl; }
+            }
+        }
     }
-}}
 
 
-	return 0;
+    return 0;
 
 }
